695b8732159a383cd04a31407725711eaef1ec90
zidane sultansyah <pepensutisna2@gmail.com>
2020-12-30 18:29:32 +0700

    mengupload tugas besar

---
+++ android/app/src/main/kotlin/com/example/kopiku_kelompok11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kopiku_kelompok11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:kopiku_kelompok11/homePage.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startSplashScreen();
+    //Timer(Duration(seconds: 5), () => print("Splash Selesai!"));
+  }
+  startSplashScreen () async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, (){
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage ()
+        )
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.brown)
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.free_breakfast,
+                          color: Colors.brown,
+                          size: 60.0,
+                        )
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      Text(
+                         "Kopiku",
+                         style: TextStyle(
+                           color: Colors.white,
+                           fontSize: 22.0,
+                           fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Padding(padding: EdgeInsets.only(top: 20.0),
+                  ),
+                  Text("Loading...",
+                  style: TextStyle(color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold)),
+                ],
+              ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
