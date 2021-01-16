485dc413595e30a0753e62c2707172f2a8c36037
novianasaila <76469937+novianasaila@users.noreply.github.com>
2021-01-16 10:00:33 +0700

    Update ortherPage.dart

--- lib/component/ortherPage.dart
+++ lib/component/ortherPage.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:kopiku_kelompok11/model/coffeModelOrther.dart';
+import 'package:kopiku_kelompok11/component/menuItemOrther.dart';
 
 class OrtherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-      child: Center(
-        child: Text("Orther page"),
+      child: ListView.builder(
+        itemCount: menu3.length,
+        itemBuilder: (context, int key) {
+          return MenuItemOrther(index3: key);
+        },
       ),
     );
   }
